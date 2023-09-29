c0d4a9411dc71dfa7e4465384c0f2ad2693becaa
spragul <spragul33@gmail.com>
2023-09-29 20:56:51 +0530

    commit

--- src/backend link.js
+++ src/backend link.js
@@ -1,2 +1,2 @@
-// export const URL='https://tournament-5rg7.onrender.com';
-export const URL='http://localhost:9000';
+export const URL='https://tournament-5rg7.onrender.com';
+// export const URL='http://localhost:9000';
